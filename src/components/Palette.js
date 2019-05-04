fde60f7b5690c0a094f1e2e4e51e605f6722b0b0
Chase Harrison <ChaseH88@gmail.com>
2019-05-04 16:25:13 -0500

    Adding to Git

---
+++ src/components/Palette.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+
+// Components
+import ColorBox from './ColorBox';
+
+const PaletteWrap = styled.div`
+  height: 100vh;
+  & .colors {
+    height: 90%;
+  }
+`;
+
+const Palette = (props) => {
+  const colorBoxes = props.colors.map(c => (
+    <ColorBox background={c.color} name={c.name} />
+  ))
+  return (
+    <PaletteWrap>
+    {/* Nav bar here */}
+      <div className="colors">
+        {colorBoxes}
+      </div>
+      {/* footer here */}
+    </PaletteWrap>
+  );
+}
+
+export default Palette;
